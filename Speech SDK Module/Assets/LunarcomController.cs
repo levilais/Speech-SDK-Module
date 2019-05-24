ed4ed4eb9e88a971d23b3468e675d24320d09e63
Levi Lais <levilais@gmail.com>
2019-05-24 14:28:58 -0700

    Added UI

---
+++ Speech SDK Module/Assets/LunarcomController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LunarcomController : MonoBehaviour
+{
+    // Key 1: 37b71e1e68fd497aa06367bb75bd2351
+    // Key 2: febaa5534609486b852704fcffbf1d2a
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Speech SDK Module/Assets/SpriteStateSwapper.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpriteStateSwapper : MonoBehaviour
+{
+    public Button button;
+    public Sprite Mic_Default;
+    public Sprite Mic_Activated;
+    bool isSelected = false;
+
+    public void ToggleSelected()
+    {
+        if (isSelected)
+        {
+            button.image.sprite = Mic_Default;
+            isSelected = false;
+        } else
+        {
+            button.image.sprite = Mic_Activated;
+            isSelected = true;
+        }
+    }
+}
