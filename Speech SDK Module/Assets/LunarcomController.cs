7b954c6966c48a98804bb0d509ab9edc4a422f66
Developer-OI <50341621+Developer-OI@users.noreply.github.com>
2019-05-24 15:50:14 -0700

    MRTK Import

--- Speech SDK Module/Assets/LunarcomController.cs
+++ Speech SDK Module/Assets/LunarcomController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class LunarcomController : MonoBehaviour
 {
+    // https://westus.api.cognitive.microsoft.com/sts/v1.0
     // Key 1: 37b71e1e68fd497aa06367bb75bd2351
     // Key 2: febaa5534609486b852704fcffbf1d2a
 
